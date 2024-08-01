7a2277e1d210043132ae2b74464c352ac2a300be
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-08-01 09:21:19 +0530

    edit

--- main.go
+++ main.go
@@ -6,20 +6,21 @@ import (
 )
 
 func main() {
+	// Nomor 1
 	fmt.Println(modules.Pembulatan(4.37))
 	fmt.Println(modules.Pembulatan(4.32))
 	fmt.Println(modules.Pembulatan(4.324))
 
-	deret := modules.DeretBilang{Value: 40}
+	// Nomor 2
+	// deret := modules.DeretBilang{Value: 40}
+	// fmt.Println("Bilangan Prima:", deret.Prima())
+	// fmt.Println("Bilangan Ganjil:", deret.Ganjil())
+	// fmt.Println("Bilangan Genap:", deret.Genap())
+	// fmt.Println("Deret Fibonacci:", deret.Fibonacci())
 
-	fmt.Println("Bilangan Prima:", deret.Prima())
-	fmt.Println("Bilangan Ganjil:", deret.Ganjil())
-	fmt.Println("Bilangan Genap:", deret.Genap())
-	fmt.Println("Deret Fibonacci:", deret.Fibonacci())
-
-	box := &modules.Box{Panjang: 3, Lebar: 4, Tinggi: 5}
-
-    fmt.Printf("Luas: %f\n", box.Luas())
-    fmt.Printf("Keliling: %f\n", box.Keliling())
-    fmt.Printf("Volume: %f\n", box.Volume())
+	// Nomor 3
+	// box := &modules.Box{Panjang: 3, Lebar: 4, Tinggi: 5}
+    // fmt.Printf("Luas: %.2f\n", box.Luas())
+    // fmt.Printf("Keliling: %.2f\n", box.Keliling())
+    // fmt.Printf("Volume: %.2f\n", box.Volume())
 }
